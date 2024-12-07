374c28ca5bbb94434f774f77f38faa9cae3e77dd
katzklaw <86386655+katzklaw@users.noreply.github.com>
2024-12-06 20:27:15 -0600

    Update LocalOptionsHolder.cs

--- BetterVanilla/Core/LocalOptionsHolder.cs
+++ BetterVanilla/Core/LocalOptionsHolder.cs
@@ -11,8 +11,7 @@ public sealed class LocalOptionsHolder
     public readonly BoolLocalOption DisplayVotesAfterDeath;
     public readonly BoolLocalOption DisplayVoteColorsAfterDeath;
     public readonly BoolLocalOption AutoPlayAgain;
-    public readonly StringLocalOption TeamPreference;
-
+    
     public readonly BoolLocalOption AllowModdedCosmetics;
     public readonly BoolLocalOption DisableAmDeadCheck;
     public readonly BoolLocalOption DisableAmImpostorCheck;
@@ -36,29 +35,14 @@ public sealed class LocalOptionsHolder
         DisableAmImpostorCheck = category.CreateBool("DisableAmImpostorCheck", "Disable Am Impostor Check", false);
         DisableEndGameChecks = category.CreateBool("DisableEndGameChecks", "Disable All End Game Checks", false);
         DisableGameStartRequirement = category.CreateBool("DisableGameStartRequirement", "Disable Start Game Player Requirement", false);
-        ForcedTeamAssignment = category.CreateEnum("ForcedTeamAssignment", "Forced Team Assignment", TeamPreferences.Both);
-        
+                
         AllowModdedCosmetics.LockWithPassword("BBB7513CC9488F1FFBCF53A294B3D15FACF4457BC6019B8200A48952232BCC59");
         DisableAmDeadCheck.LockWithPassword("FD1C2CCC2801F102F08A0D72724813B8EF7BD8CBA50E145816AF9BFD917CF31E");
         DisableAmImpostorCheck.LockWithPassword("FD1C2CCC2801F102F08A0D72724813B8EF7BD8CBA50E145816AF9BFD917CF31E");
         DisableEndGameChecks.LockWithPassword("FD1C2CCC2801F102F08A0D72724813B8EF7BD8CBA50E145816AF9BFD917CF31E");
         DisableGameStartRequirement.LockWithPassword("FD1C2CCC2801F102F08A0D72724813B8EF7BD8CBA50E145816AF9BFD917CF31E");
-        ForcedTeamAssignment.LockWithPassword("6C86869BCDDD51E006515334CCBD1CD2F374ADC7369D28F69C29E24D4D0B9EE0");
-
-        TeamPreference.ValueChanged += OnTeamPreferenceValueChanged;
-        ForcedTeamAssignment.ValueChanged += OnForcedTeamAssignmentValueChanged;
+        
     }
 
-    private void OnTeamPreferenceValueChanged()
-    {
-        if (!PlayerControl.LocalPlayer || !AmongUsClient.Instance) return;
-        PlayerControl.LocalPlayer.RpcSetTeamPreference(TeamPreference.ParseValue(TeamPreferences.Both));
-    }
 
-    private void OnForcedTeamAssignmentValueChanged()
-    {
-        if (!PlayerControl.LocalPlayer || !AmongUsClient.Instance) return;
-
-        PlayerControl.LocalPlayer.RpcSetForcedTeamAssignment(ForcedTeamAssignment.ParseValue(TeamPreferences.Both));
-    }
 }
